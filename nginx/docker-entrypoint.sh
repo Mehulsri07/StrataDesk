ab9c2dd1563d68543ac2b3a70926bd365c23c609
Mehul <meulsri.0701@gmail.com>
2026-05-16 19:19:31 +0530

    fix: LF line endings on entrypoint.sh for Alpine Linux

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+﻿#!/bin/sh
 # Substitute API_UPSTREAM placeholder with the real value from the environment.
 # Local Docker Compose: API_UPSTREAM defaults to "api:3001"
 # Railway:              set API_UPSTREAM to "<api-service>.railway.internal:3001"
@@ -7,6 +7,6 @@ API_UPSTREAM="${API_UPSTREAM:-api:3001}"
 PORT="${PORT:-80}"
 
 sed -i "s|API_UPSTREAM|${API_UPSTREAM}|g" /etc/nginx/nginx.conf
-sed -i "s|\${PORT:-80}|${PORT}|g"         /etc/nginx/nginx.conf
+sed -i "s|${PORT:-80}|${PORT}|g"         /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
